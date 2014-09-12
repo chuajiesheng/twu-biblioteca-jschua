0d46efaa8e849dc5fe2effb095226b04ea3cc427
chuajiesheng <chuajiesheng@f6dc0fb2-9825-3602-0f76-286cddcfc116>
2014-09-12 05:03:38 +0000

    Added listAllBooks method and test

--- trunk/src/com/twu/biblioteca/BibliotecaApp.java
+++ trunk/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,22 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    public static final String ALL_BOOKS = "1. Steve Jobs by Walter Isaacson\n" +
+            "2. Thinking, Fast and Slow by Daniel Kahneman\n" +
+            "3. Brave New World by Aldous Huxley";
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(generateWelcomeMessage());
+        System.out.println("\n");
+        System.out.println("List of all library books:");
+        System.out.println(listAllBooks());
     }
 
-    public String generateWelcomeMessage() {
+    public static String generateWelcomeMessage() {
         return "Welcome to Biblioteca!";
+    }
+
+    public static String listAllBooks() {
+        return ALL_BOOKS;
     }
 }
--- trunk/test/com/twu/biblioteca/BibliotecaAppTest.java
+++ trunk/test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -12,5 +13,11 @@ public class BibliotecaAppTest {
     public void testGenerateWelcomeMessage() throws Exception {
         String expected = "Welcome to Biblioteca!";
         assertEquals(expected, new BibliotecaApp().generateWelcomeMessage());
+    }
+
+    @Test
+    public void testListAllBooks() throws Exception {
+        String expected = BibliotecaApp.ALL_BOOKS;
+        assertEquals(expected,  new BibliotecaApp().listAllBooks());
     }
 }
