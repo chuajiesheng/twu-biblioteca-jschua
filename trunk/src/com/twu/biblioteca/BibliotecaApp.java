b4eaccf257a61765aa2e1f75bafe89a4f258b226
chuajiesheng <chuajiesheng@f6dc0fb2-9825-3602-0f76-286cddcfc116>
2014-09-12 05:32:40 +0000

    Implement menu handling in main

--- trunk/src/com/twu/biblioteca/BibliotecaApp.java
+++ trunk/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,15 +1,23 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
     public static final String ALL_BOOKS = "1. Steve Jobs\t\t\t\tWalter Isaacson\t2011\n" +
             "2. Thinking, Fast and Slow\tDaniel Kahneman\t2011\n" +
             "3. Brave New World\t\t\tAldous Huxley\t2006";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(generateWelcomeMessage());
         System.out.println("\n");
-        System.out.println("List of all library books:");
-        System.out.println(listAllBooks());
+        System.out.println(showMenu());
+
+        while (sc.hasNext()) {
+            int option = Integer.parseInt(sc.next());
+            System.out.println(navigateMenu(option));
+        }
     }
 
     public static String generateWelcomeMessage() {
