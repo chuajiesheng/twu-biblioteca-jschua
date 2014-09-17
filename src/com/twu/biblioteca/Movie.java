d00bd0878665faf7aa6d65a2d78a70251d70d511
chuajiesheng <chuajiesheng@f6dc0fb2-9825-3602-0f76-286cddcfc116>
2014-09-17 16:00:53 +0000

    Implemented movieCheckOut test and method

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,65 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by chuajiesheng on 17/9/14.
+ */
+public class Movie {
+    private int id;
+    private String name;
+    private int year;
+    private String director;
+    private int rating;
+
+    public Movie(int id, String name, int year, String director, int rating) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%2d. %-25s (%4d) %-12s %-2d", id, name, year, director, rating);
+    }
+}
